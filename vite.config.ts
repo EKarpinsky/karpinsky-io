da6132c1783168b1a4626f17dbb4fb5a16b48cd2
Eli Karpinsky <ekarpinsky1@gmail.com>
2024-06-14 18:39:04 -0400

    Update Svelte adapter and improve CSS and build settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		sourcemap: process.env.NODE_ENV !== 'production'
+	},
+	css: {
+		devSourcemap: process.env.NODE_ENV !== 'production'
+	}
 });
